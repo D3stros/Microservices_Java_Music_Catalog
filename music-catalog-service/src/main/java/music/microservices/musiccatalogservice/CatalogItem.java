0cd1a8e4ec266b3ee45cfa48388e07522895cf79
D3stros <patrick.fallmann@hotmail.com>
2020-07-30 23:53:08 +0200

    Building music catalog service API

---
+++ music-catalog-service/src/main/java/music/microservices/musiccatalogservice/CatalogItem.java
@@ -0,0 +1,37 @@
+package music.microservices.musiccatalogservice;
+
+public class CatalogItem {
+    private String name;
+    private String genre;
+    private int rating;
+
+    public CatalogItem(String name, String genre, int rating) {
+        this.name = name;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
---
+++ music-catalog-service/src/main/java/music/microservices/musiccatalogservice/MusicCatalogResource.java
@@ -0,0 +1,21 @@
+package music.microservices.musiccatalogservice;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MusicCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId")String userId) {
+
+        return Collections.singletonList(
+                new CatalogItem("Thriller", "Pop", 8)
+        );
+    }
+}
