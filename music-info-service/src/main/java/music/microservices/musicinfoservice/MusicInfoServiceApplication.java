6eab47e1cef785b6de3ec56238796fd400e01752
D3stros <patrick.fallmann@hotmail.com>
2020-07-31 18:16:51 +0200

    Created Eureka server and clients

--- music-info-service/src/main/java/music/microservices/musicinfoservice/MusicInfoServiceApplication.java
+++ music-info-service/src/main/java/music/microservices/musicinfoservice/MusicInfoServiceApplication.java
@@ -2,8 +2,10 @@ package music.microservices.musicinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MusicInfoServiceApplication {
 
 	public static void main(String[] args) {
